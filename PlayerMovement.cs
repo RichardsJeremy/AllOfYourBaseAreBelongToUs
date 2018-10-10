608877547157a1fa45285f0f88914991428f4b94
RichardsJeremy <42659289+RichardsJeremy@users.noreply.github.com>
2018-10-10 21:00:21 +1100

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Jeremy Richards S3721762
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed;
+    private Vector2 newDirection;
+    void Update()
+    {
+        Vector3 currentPos = gameObject.transform.position;
+        int destinationX = 0;
+        int destinationY = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            destinationY = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            destinationX = -1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            destinationY = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            destinationX = 1;
+        }
+        float xMove = currentPos.x + destinationX;
+        float yMove = currentPos.y + destinationY;
+        Vector2 newPos = new Vector2(
+            xMove,
+            yMove
+            );
+        gameObject.transform.position = newPos;
+    }
+}
