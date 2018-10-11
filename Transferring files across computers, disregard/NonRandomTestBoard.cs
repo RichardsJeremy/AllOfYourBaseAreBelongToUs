052e153b2218671c0f190e564df3d533345fe455
Orlando Jazz Fitzgerald-Leonard <s3670970@student.rmit.edu.au>
2018-10-11 22:29:29 +1100

    Transferring working files to different computer
    Transferring working files to different computer

---
+++ Transferring files across computers, disregard/NonRandomTestBoard.cs
@@ -0,0 +1,101 @@
+﻿using System;
+namespace SEF_AssIgnment1
+{
+    public class NonRandomTestBoard
+    {
+        Random random = new Random();
+
+        public NonRandomTestBoard()
+        {
+
+        }
+
+        private void CreateTestBoard()
+        {
+            Create2DArray();
+        }
+        private int[,] Create2DArray()
+        {
+            //This creates the test board. A prebuilt maze for pacman.
+            //Monsters starts at (6,7) & (8,7)
+            //0,0 is top left.
+            int[,] board = new int[,]{
+
+                { 1,4,4,4,4,4,4,4,4,4,4,4,4,4,1},
+                { 6,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
+                { 6,0,1,1,1,0,1,1,1,0,1,1,1,0,7},
+                { 6,0,1,0,0,0,1,1,1,0,0,0,1,0,7},
+                { 6,0,1,0,1,0,1,1,1,0,1,0,1,0,7},
+                { 6,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
+                { 6,0,1,0,0,1,1,0,1,1,0,0,1,0,7},
+                { 6,0,1,0,1,1,0,0,0,1,1,0,1,0,7},
+                { 6,0,1,0,0,1,1,1,1,1,0,0,1,0,7},
+                { 6,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
+                { 6,0,1,0,1,0,1,1,1,0,1,0,1,0,7},
+                { 6,0,1,0,0,0,1,1,1,0,0,0,1,0,7},
+                { 6,0,1,1,1,0,1,1,1,0,1,1,1,0,7},
+                { 6,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
+                { 1,5,5,5,5,5,5,5,5,5,5,5,5,5,1},
+
+            };
+
+            //This replaces all empty spaces with coins.
+            for (int y = 0; y < 14; y++)
+            {
+                for (int x = 0; x < 14; x++)
+                {
+                    if (board[x, y] == 0)
+                    {
+                        board[x, y] = 3;
+                    }
+                }
+            }
+
+            //This will ensure that 7,13 remains open for player to spawn
+            //by waiting until all the empty spaces are made coins,
+            //and then removing the one coin from the spawn location.
+            //This will also clear the monster spawn points, although they
+            //cannot pick up coins anyway.
+            board[7, 13] = 0; //Player Spawn
+            board[6, 7] = 0; //Monster 1
+            board[8, 7] = 0; //Monster 2
+
+            //I will generalize spawn points so that it will work to inputable values
+
+            //I will also allow for maze internals to be randomized by iterating
+            //over the array, and when I find a zero, I will roll a random number
+            //selecting from an empty space, a coin, or a block.
+
+            return board;
+        }
+
+        public void FruitOnMap(int[,] board)
+        {
+            int fruitsOnMapATM = 0;
+            int randomFruitChance = 0;
+
+            for (int y = 0; y < 14; y++)
+            {
+                for (int x = 0; x < 14; x++)
+                {
+                    if (board[x, y] == 2)
+                    {
+                        fruitsOnMapATM ++;
+                    }
+
+                    if (fruitsOnMapATM<2 && board[x,y] /= 2)
+                    {
+                        randomFruitChance = random.Next(0, 40);
+
+                        if(randomFruitChance == 16) //1 in 40 chance
+                        {
+                            board[x, y] = 2;
+
+                            fruitsOnMapATM++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Transferring files across computers, disregard/TestCase1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace SEF_AssIgnment1
+{
+    public class TestCase1
+    {
+        Random random = new Random();
+
+        int boardHeight = 15;
+        int boardWidth = 15;        
+
+        public void TestCase1_AssignRandomValueToTile(int boardWidth)
+        {
+            int[] tileRow1 = new int[boardWidth];
+            foreach (int i in tileRow1)
+            {
+                tileRow1[i] = randomize();
+                foreach (int x in tileRow1)
+                {
+                    System.Console.Write("{0} ", x);
+                }
+            }
+
+        }
+        private int randomize()
+        {
+            //Here I am seting up the percentages for the random tile function.
+            //This function will be called as the loop above iterates across the
+            //length of the row array.
+
+            //The options will be open block (20%), obstruction (40%), & coin (40%).
+            //Fruit will be called later.
+            int[] randomTileOption = new int[10];
+
+            randomTileOption[0] = 0;
+            randomTileOption[1] = 1;
+            randomTileOption[2] = 3;
+            randomTileOption[3] = 1;
+            randomTileOption[4] = 3;
+            randomTileOption[5] = 1;
+            randomTileOption[6] = 3;
+            randomTileOption[7] = 1;
+            randomTileOption[8] = 3;
+            randomTileOption[9] = 0;
+
+            int randomTile = randomTileOption[randomRoll()];
+
+            return randomTile;
+        }
+
+        private int randomRoll()
+        {
+            //Here I will simulate a roll to emulate a more random approach.
+            //There will be 5 rolls, with an equal chance of rolling 0, 1, or 2.
+
+            int totalRoll = 0;
+            int rollTimes = 1;
+
+            while (rollTimes < 6) /*iterate 5 times*/
+            {
+
+                int diceRoll = random.Next(0, 3);
+                totalRoll = totalRoll + diceRoll;
+                rollTimes++;
+            }
+
+            return totalRoll;
+        }
+
+    }
+}
